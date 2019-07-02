1d3786b82e798b70d4a6f5ebdce8520e6ed7f063
MO\vladimir.bobrov <vladimir.bobrov@anywayanyday.com>
2019-07-02 18:44:10 +0300

    Инициализация, создание

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.dev_intensive_2019
+package ru.skillbranch.devintensive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dev_intensive_2019.R
 
 class MainActivity : AppCompatActivity() {
 

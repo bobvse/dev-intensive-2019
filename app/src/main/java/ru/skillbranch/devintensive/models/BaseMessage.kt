369e2ab3d2605f633f30bc2095bb8d83df68608c
MO\vladimir.bobrov <vladimir.bobrov@anywayanyday.com>
2019-07-09 12:36:01 +0300

    фиксы

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -2,49 +2,24 @@ package ru.skillbranch.devintensive.models
 
 import java.util.*
 
-abstract class BaseMessage(
+abstract class BaseMessage (
         val id: String,
         val from: User?,
         val chat: Chat,
         val isIncoming: Boolean = false,
-        val date: Date = Date()
+        val date : Date = Date()
 ) {
-    /**
-     * возвращает строку содержащюю информацию о
-     * id сообщения,µ
-     * имени получателя/отправителя,
-     * виде сообщения ("получил/отправил") и
-     * типе сообщения ("сообщение"/"изображение")
-     */
+
     abstract fun formatMessage(): String
 
-    companion object AbstractFactory {
-        private var messageId: Int = -1
-        fun makeMessage(
-                from: User?,
-                chat: Chat,
-                date: Date,
-                type: String = "text",
-                payload: Any?,
-                isIncoming: Boolean = false
-        ): BaseMessage {
-            return when (type) {
-                "image" -> ImageMessage(
-                        "${++messageId}",
-                        from,
-                        chat,
-                        isIncoming,
-                        date = date,
-                        image = payload as String
-                )
-                else -> TextMessage(
-                        "${++messageId}",
-                        from,
-                        chat,
-                        isIncoming,
-                        date = date,
-                        text = payload as String
-                )
+    companion object AbstractFactory{
+        var lastId = 0
+        fun makeMessage(from: User?, chat: Chat, date: Date = Date(), type:String = "text", payload: Any, isIncoming: Boolean = false):BaseMessage{
+            return when(type){
+                "image"-> ImageMessage("${lastId++}", from, chat, date = date, image = payload as String, isIncoming = isIncoming)
+                "text" -> TextMessage("${lastId++}", from, chat, date = date, text = payload as String, isIncoming = isIncoming)
+
+                else-> if ("image" == payload || "text" == payload) makeMessage(from, chat, date, payload, type, isIncoming) else throw IllegalArgumentException()
             }
         }
     }
